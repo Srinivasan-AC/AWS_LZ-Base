4a8a475465a74b6dccb29479554444980b36b25f
dk00932866 <dk00932866@techmahindra.com>
2023-12-06 13:50:27 +0530

    redeploy

--- terraform/resources/1_application/app-vpc-a.tf
+++ terraform/resources/1_application/app-vpc-a.tf
@@ -30,7 +30,7 @@ module "app_vpc_a" {
   create_igw = var.app_vpc_a_create_igw
 
   # TGW required or not
-  tgw_not_required = var.app_vpc_a_tgw_not_required
+  #tgw_not_required = var.app_vpc_a_tgw_not_required
 
   # NAT Gateway
   enable_nat_gateway = var.app_vpc_a_enable_nat_gateway
@@ -72,17 +72,17 @@ module "app_vpc_a" {
 
 }
 
-resource "aws_route" "app_vpc_a_tgw_route" {
-  count                  = length(module.app_vpc_a.public_route_table_ids)
-  route_table_id         = module.app_vpc_a.public_route_table_ids[count.index]
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = module.tgw.ec2_transit_gateway_id
+# resource "aws_route" "app_vpc_a_tgw_route" {
+#   count                  = length(module.app_vpc_a.public_route_table_ids)
+#   route_table_id         = module.app_vpc_a.public_route_table_ids[count.index]
+#   destination_cidr_block = "0.0.0.0/0"
+#   transit_gateway_id     = module.tgw.ec2_transit_gateway_id
 
-  depends_on = [
-    module.tgw,
-    module.app_vpc_a,
-  ]
-}
+#   depends_on = [
+#     module.tgw,
+#     module.app_vpc_a,
+#   ]
+# }
 
 # resource "aws_route" "app_vpc_a_tgw_route_private" {
 #   count                  = length(module.app_vpc_a.private_route_table_ids)
--- terraform/resources/1_application/aws-inspector.tf
+++ terraform/resources/1_application/aws-inspector.tf
@@ -1,13 +1,13 @@
-module "inspector" {
-  source = "./modules/aws-inspector"
+# module "inspector" {
+#   source = "./modules/aws-inspector"
 
-  create_iam_role                        = var.create_iam_role
-  iam_role_name                          = var.iam_role_name
-  iam_policy_name                        = var.iam_policy_name
-  aws_cloudwatch_event_rule_name         = var.aws_cloudwatch_event_rule_name
-  aws_inspector_assessment_target_name   = var.aws_inspector_assessment_target_name
-  aws_inspector_assessment_template_name = var.aws_inspector_assessment_template_name
-  enabled_rules                          = var.enabled_rules
-  tags                                   = local.tags
+#   create_iam_role                        = var.create_iam_role
+#   iam_role_name                          = var.iam_role_name
+#   iam_policy_name                        = var.iam_policy_name
+#   aws_cloudwatch_event_rule_name         = var.aws_cloudwatch_event_rule_name
+#   aws_inspector_assessment_target_name   = var.aws_inspector_assessment_target_name
+#   aws_inspector_assessment_template_name = var.aws_inspector_assessment_template_name
+#   enabled_rules                          = var.enabled_rules
+#   tags                                   = local.tags
 
-}
+# }
