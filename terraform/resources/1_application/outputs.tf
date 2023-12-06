9b7b47e3e68c06591c071668156de2e0e90d8dc1
dk00932866 <dk00932866@techmahindra.com>
2023-12-06 14:34:12 +0530

    aws inspector

--- terraform/resources/1_application/outputs.tf
+++ terraform/resources/1_application/outputs.tf
@@ -391,25 +391,25 @@ output "cloudtrail_bucket_arn" {
 # AWS inspector outputs
 ######################################################################################
 
-# output "inspector_assessment_target" {
-#   description = "The AWS Inspector assessment target"
-#   value       = module.inspector.inspector_assessment_target
-# }
+output "inspector_assessment_target" {
+  description = "The AWS Inspector assessment target"
+  value       = module.inspector.inspector_assessment_target
+}
 
-# output "aws_inspector_assessment_template" {
-#   description = "The AWS Inspector assessment template"
-#   value       = module.inspector.aws_inspector_assessment_template
-# }
+output "aws_inspector_assessment_template" {
+  description = "The AWS Inspector assessment template"
+  value       = module.inspector.aws_inspector_assessment_template
+}
 
-# output "aws_cloudwatch_event_rule" {
-#   description = "The AWS Inspector event rule"
-#   value       = module.inspector.aws_cloudwatch_event_rule
-# }
+output "aws_cloudwatch_event_rule" {
+  description = "The AWS Inspector event rule"
+  value       = module.inspector.aws_cloudwatch_event_rule
+}
 
-# output "aws_cloudwatch_event_target" {
-#   description = "The AWS Inspector event target"
-#   value       = module.inspector.aws_cloudwatch_event_target
-# }
+output "aws_cloudwatch_event_target" {
+  description = "The AWS Inspector event target"
+  value       = module.inspector.aws_cloudwatch_event_target
+}
 
 ################################################################################
 # AWS Security Hub Outputs
