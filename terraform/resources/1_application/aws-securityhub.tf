56ced2e71420676d7ac936c6484289476641edd3
dk00932866 <dk00932866@techmahindra.com>
2023-12-06 15:32:48 +0530

    redeploy

--- terraform/resources/1_application/aws-securityhub.tf
+++ terraform/resources/1_application/aws-securityhub.tf
@@ -1,10 +1,10 @@
-module "security_hub" {
-  source = "./modules/aws-security-hub"
+# module "security_hub" {
+#   source = "./modules/aws-security-hub"
 
-  enabled_standards = var.enabled_standards
-  create_sns_topic  = var.create_sns_topic
-  sns_topic_name    = var.sns_topic_name
-  sns_topic_tags    = var.sns_topic_tags
+#   enabled_standards = var.enabled_standards
+#   create_sns_topic  = var.create_sns_topic
+#   sns_topic_name    = var.sns_topic_name
+#   sns_topic_tags    = var.sns_topic_tags
 
 
-}
+# }
