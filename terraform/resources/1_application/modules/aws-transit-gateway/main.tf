a7d3a5b88d47a6c92e18d41ff966a33e66156c86
dk00932866 <dk00932866@techmahindra.com>
2024-01-12 17:50:35 +0530

    cleanup

--- terraform/resources/1_application/modules/aws-transit-gateway/main.tf
+++ terraform/resources/1_application/modules/aws-transit-gateway/main.tf
@@ -161,12 +161,12 @@ resource "aws_ram_resource_association" "this" {
   resource_share_arn = aws_ram_resource_share.this[0].id
 }
 
-resource "aws_ram_principal_association" "this" {
-  count = var.create_tgw && var.share_tgw ? length(var.ram_principals) : 0
+# resource "aws_ram_principal_association" "this" {
+#   count = var.create_tgw && var.share_tgw ? length(var.ram_principals) : 0
 
-  principal          = var.ram_principals[count.index]
-  resource_share_arn = aws_ram_resource_share.this[0].arn
-}
+#   principal          = var.ram_principals[count.index]
+#   resource_share_arn = aws_ram_resource_share.this[0].arn
+# }
 
 resource "aws_ram_resource_share_accepter" "this" {
   count = !var.create_tgw && var.share_tgw ? 1 : 0
